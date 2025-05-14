b54098a9eb636e3ba1c39270f9bcf748fa7763c0
reddy-sravan12 <101791343+reddy-sravan12@users.noreply.github.com>
2025-05-14 18:52:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 function firstNonRepeatedChar(str) {
- // Write your code here
+  const mapper=new Map()
+  for (let char=0; char< str.length;char++){
+    //   console.log(mapper.has(str[char]))
+      if(!mapper.has(str[char])){
+          mapper.delete(str[char])
+      }
+      mapper.set(str[char],(mapper.get(str[char])||0)+1)
+  }
+  for (let char of mapper){
+      if(char[1]===1){
+          return char[0]
+      }
+  }
+  return null
 }
+
+firstNonRepeatedChar('aabbc')
+
+
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
